1d71164f38c160671cd1d3b069b106e7e9d877dc
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-23 20:00:16 -0300

    TextInput

--- lab-ds/src/components/TextInput.tsx
+++ lab-ds/src/components/TextInput.tsx
@@ -1,10 +1,60 @@
-import { InputHTMLAttributes } from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { InputHTMLAttributes, ReactNode } from "react";
 
-export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
+
+export interface TextInputRootProps {
+  children: ReactNode
 }
 
-export function TextInput({ ...rest }: TextInputProps) {
+function TextInputRoot({ children }: TextInputRootProps) {
   return (
-    <input className='py-4 px-3 rounded bg-gray-800 text-gray-100 text-xs placeholder:text-gray-400' {...rest} />
+    <div className='
+      flex
+      items-center
+      gap-3
+      py-4 
+      px-3 
+      rounded 
+      bg-gray-800 
+      focus-within:ring-2
+      ring-cyan-300
+      '>
+      {children}
+    </div>
   )
+}
+
+TextInputRoot.displayName = 'TextInput.Root'
+
+export interface TextInputIconProps {
+  children: ReactNode
+}
+
+function TextInputIcon({ children }: TextInputIconProps) {
+  return (
+    <Slot className="w-6 h-6 text-gray-400">
+      {children}
+    </Slot>
+  )
+}
+
+TextInputIcon.displayName = 'TextInput.Icon'
+
+
+export interface TextInputInputProps extends InputHTMLAttributes<HTMLInputElement> { }
+
+
+function TextInputInput({ ...rest }: TextInputInputProps) {
+  return (
+    <input className="bg-transparent flex-1  text-gray-100 text-xs placeholder:text-gray-400 outline-none"  {...rest} />
+  )
+}
+
+TextInputInput.displayName = 'TextInput.Input'
+
+
+export const TextInput = {
+  Root: TextInputRoot,
+  Input: TextInputInput,
+  Icon: TextInputIcon,
 }
