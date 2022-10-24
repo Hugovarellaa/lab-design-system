e8e16331d1e6b883c56718bd818ec40f6ae74b35
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-24 19:54:52 -0300

    Component Button

---
+++ src/components/Button.stories.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line prettier/prettier
+import { Meta, StoryObj } from '@storybook/react';
+import { Button, ButtonProps } from './Button';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+  args: {
+    children: 'Create Account',
+  },
+  argTypes: {}
+} as Meta<ButtonProps>
+
+export const Default: StoryObj<ButtonProps> = {
+  argTypes: {
+
+    asChild: {
+      table: {
+        disable: true
+      }
+    }
+  }
+}
