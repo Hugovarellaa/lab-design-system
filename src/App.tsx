e73e8ef5f3568c7ffe1a8d38c098097b770f29ea
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-24 17:04:41 -0300

    Fonts

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import './styles/main.css'
 
 export function App() {
   return (
-    <div>
-      <h1>
-        ola mundo
-      </h1>
+    <div className='flex flex-col items-center justify-center w-screen h-screen'>
+      <h1 className='font-bold text-5xl text-violet-500'>Hello world</h1>
+
+      <button className='bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-700'>
+        Enviar
+      </button>
     </div>
   )
 }
