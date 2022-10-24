2d26f5d78a6f6b9713207b8681f70602d61a229a
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-24 20:56:59 -0300

    Component TextInput

---
+++ src/components/TextInput.stories.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line prettier/prettier
+import { Meta, StoryObj } from '@storybook/react';
+import { Envelope } from 'phosphor-react';
+import { TextInput, TextInputRootProps } from './TextInput';
+
+export default {
+  title: 'Components/TextInput',
+  component: TextInput.TextInputRoot,
+  args: {
+    children: [
+      <TextInput.TextInputIcon>
+        <Envelope />
+      </TextInput.TextInputIcon>,
+      <TextInput.TextInputInput placeholder='Type your e-mail address' />
+    ]
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true
+      }
+    }
+  }
+} as Meta<TextInputRootProps>
+
+export const Default: StoryObj<TextInputRootProps> = {
+
+}
